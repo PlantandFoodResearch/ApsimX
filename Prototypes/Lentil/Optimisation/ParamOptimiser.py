7576acf3eb8b4353b14e754832584efa803139e3
Hamish Brown <hamish.brown@plantandfood.co.nz>
2024-11-28 17:09:07 +1300

    Optimisation working

--- Prototypes/Lentil/Optimisation/ParamOptimiser.py
+++ Prototypes/Lentil/Optimisation/ParamOptimiser.py
@@ -13,189 +13,40 @@
 #     name: python3
 # ---
 
-# +
 import datetime as dt
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import APSIMGraphHelpers as AGH
-import GraphHelpers as GH
-from scipy import stats
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-import matplotlib.dates as mdates
 import MathsUtilities as MUte
-import shlex # package to construct the git command to subprocess format
 import subprocess 
-#import ProcessWheatFiles as pwf
 import xmltodict, json
 import sqlite3
-import scipy.optimize 
 from skopt import gp_minimize
 from skopt.callbacks import CheckpointSaver
 from skopt import load
 from skopt.plots import plot_convergence
 import matplotlib.gridspec as gridspec
 import time
-
-import winsound
-frequency = 2500  # Set Frequency To 2500 Hertz
-duration = 1000  # Set Duration To 1000 ms == 1 second
+import os
 # %matplotlib inline
-# -
-
-BaseSimNames =['Roberts1988InitT1oCInitPp8hDurat0FinalT12oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp8hDurat10FinalT12oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp8hDurat30FinalT12oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp8hDurat60FinalT12oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp8hDurat0FinalT12oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp8hDurat10FinalT12oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp8hDurat30FinalT12oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp8hDurat60FinalT12oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp8hDurat0FinalT12oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp8hDurat10FinalT12oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp8hDurat30FinalT12oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp8hDurat60FinalT12oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp16hDurat0FinalT12oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp16hDurat10FinalT12oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp16hDurat30FinalT12oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp16hDurat60FinalT12oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp16hDurat0FinalT12oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp16hDurat10FinalT12oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp16hDurat30FinalT12oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp16hDurat60FinalT12oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp16hDurat0FinalT12oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp16hDurat10FinalT12oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp16hDurat30FinalT12oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp16hDurat60FinalT12oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp8hDurat0FinalT19oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp8hDurat10FinalT19oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp8hDurat30FinalT19oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp8hDurat60FinalT19oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp8hDurat0FinalT19oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp8hDurat10FinalT19oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp8hDurat30FinalT19oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp8hDurat60FinalT19oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp8hDurat0FinalT19oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp8hDurat10FinalT19oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp8hDurat30FinalT19oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp8hDurat60FinalT19oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp16hDurat0FinalT19oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp16hDurat10FinalT19oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp16hDurat30FinalT19oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp16hDurat60FinalT19oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp16hDurat0FinalT19oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp16hDurat10FinalT19oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp16hDurat30FinalT19oCFinalPp11hCv',
-'Roberts1988InitT5oCInitPp16hDurat60FinalT19oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp16hDurat0FinalT19oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp16hDurat10FinalT19oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp16hDurat30FinalT19oCFinalPp11hCv',
-'Roberts1988InitT9oCInitPp16hDurat60FinalT19oCFinalPp11hCv',
-'Roberts1988InitT1oCInitPp8hDurat0FinalT12oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp8hDurat10FinalT12oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp8hDurat30FinalT12oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp8hDurat60FinalT12oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp8hDurat0FinalT12oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp8hDurat10FinalT12oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp8hDurat30FinalT12oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp8hDurat60FinalT12oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp8hDurat0FinalT12oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp8hDurat10FinalT12oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp8hDurat30FinalT12oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp8hDurat60FinalT12oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp16hDurat0FinalT12oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp16hDurat10FinalT12oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp16hDurat30FinalT12oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp16hDurat60FinalT12oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp16hDurat0FinalT12oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp16hDurat10FinalT12oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp16hDurat30FinalT12oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp16hDurat60FinalT12oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp16hDurat0FinalT12oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp16hDurat10FinalT12oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp16hDurat30FinalT12oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp16hDurat60FinalT12oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp8hDurat0FinalT19oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp8hDurat10FinalT19oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp8hDurat30FinalT19oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp8hDurat60FinalT19oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp8hDurat0FinalT19oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp8hDurat10FinalT19oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp8hDurat30FinalT19oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp8hDurat60FinalT19oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp8hDurat0FinalT19oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp8hDurat10FinalT19oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp8hDurat30FinalT19oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp8hDurat60FinalT19oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp16hDurat0FinalT19oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp16hDurat10FinalT19oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp16hDurat30FinalT19oCFinalPp16hCv',
-'Roberts1988InitT1oCInitPp16hDurat60FinalT19oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp16hDurat0FinalT19oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp16hDurat10FinalT19oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp16hDurat30FinalT19oCFinalPp16hCv',
-'Roberts1988InitT5oCInitPp16hDurat60FinalT19oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp16hDurat0FinalT19oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp16hDurat10FinalT19oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp16hDurat30FinalT19oCFinalPp16hCv',
-'Roberts1988InitT9oCInitPp16hDurat60FinalT19oCFinalPp16hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax18oCFinalTmin5oCFinalPp10hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax28oCFinalTmin5oCFinalPp10hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax18oCFinalTmin13oCFinalPp10hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax28oCFinalTmin13oCFinalPp10hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax18oCFinalTmin5oCFinalPp13hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax18oCFinalTmin13oCFinalPp13hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax28oCFinalTmin5oCFinalPp13hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax28oCFinalTmin13oCFinalPp13hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax18oCFinalTmin5oCFinalPp16hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax18oCFinalTmin13oCFinalPp16hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax28oCFinalTmin5oCFinalPp16hCv',
-'Summerfield1985InitT1oCDurat0FinalTmax28oCFinalTmin13oCFinalPp16hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax18oCFinalTmin5oCFinalPp10hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax28oCFinalTmin5oCFinalPp10hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax18oCFinalTmin13oCFinalPp10hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax28oCFinalTmin13oCFinalPp10hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax18oCFinalTmin5oCFinalPp13hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax18oCFinalTmin13oCFinalPp13hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax28oCFinalTmin5oCFinalPp13hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax28oCFinalTmin13oCFinalPp13hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax18oCFinalTmin5oCFinalPp16hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax18oCFinalTmin13oCFinalPp16hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax28oCFinalTmin5oCFinalPp16hCv',
-'Summerfield1985InitT1oCDurat30FinalTmax28oCFinalTmin13oCFinalPp16hCv',
-'Roberts1986Pp10to16hDurat41Cv',
-'Roberts1986Pp10to16hDurat37Cv',
-'Roberts1986Pp10to16hDurat33Cv',
-'Roberts1986Pp10to16hDurat29Cv',
-'Roberts1986Pp10to16hDurat25Cv',
-'Roberts1986Pp10to16hDurat21Cv',
-'Roberts1986Pp10to16hDurat17Cv',
-'Roberts1986Pp10to16hDurat13Cv',
-'Roberts1986Pp10to16hDurat9Cv',
-'Roberts1986Pp10to16hDurat0Cv',
-'Roberts1986Pp16to10hDurat45Cv',
-'Roberts1986Pp16to10hDurat41Cv',
-'Roberts1986Pp16to10hDurat37Cv',
-'Roberts1986Pp16to10hDurat33Cv',
-'Roberts1986Pp16to10hDurat29Cv',
-'Roberts1986Pp16to10hDurat25Cv',
-'Roberts1986Pp16to10hDurat21Cv',
-'Roberts1986Pp16to10hDurat17Cv',
-'Roberts1986Pp16to10hDurat13Cv',
-'Roberts1986Pp16to10hDurat9Cv',
-'Roberts1986Pp16to10hDurat0Cv']
 
 # +
+Version = str(1)
+Cultivars = ['Precoz']#,'Laird','Syrian','Ethiopian']
+
+Rounds = 1
+RandomCalls = np.power(2,6)-1
+OptimizerCalls = 30
+print(str(RandomCalls) + " Random Calls will be run")
+
 BaseLentilFilePath = r"C:/GitHubRepos/ApsimX/Prototypes/Lentil/Lentil.apsimx"
 WorkingLentilFilePath = r"C:/GitHubRepos/ApsimX/Prototypes/Lentil/LentilOptItter.apsimx"
 DBFilePath = r"C:/GitHubRepos/ApsimX/Prototypes/Lentil/LentilOptItter.db"
 
 paramNames = ['[Phenology].JuvenileBase.FixedValue', 
               '[Phenology].VernSensitivity.FixedValue', 
-              '[Phenology].InductiveBase.FixedValue', 
               '[Phenology].InductivePpSensitivity.FixedValue',
-              '[Phenology].EarlyReproductiveBase.FixedValue', 
+              #'[Phenology].EarlyReproductiveBase.FixedValue', 
               '[Phenology].EarlyReproductivePpSensitivity.FixedValue']
               # '[Phenology].ThermalTime.PostEmergence.MultiplyFunction.WangEagleTempScale.Response.MinTemp',
               # '[Phenology].ThermalTime.PostEmergence.MultiplyFunction.WangEagleTempScale.Response.OptTemp',
@@ -204,8 +55,18 @@ paramNames = ['[Phenology].JuvenileBase.FixedValue',
               # '[Phenology].AccumulatedVernalisation.DailyVernalisation.Response.OptTemp',
               # '[Phenology].AccumulatedVernalisation.DailyVernalisation.Response.MaxTemp']
 
-FittingVariables = ['Lentil.Phenology.StartBuddingDAS','Lentil.Phenology.StartFloweringDAS']        
+x0 = [400.0, 0.5, 0.5, 0.5]
+bounds = [(0.0,1000.0),
+          (0.0,1.0),
+          (0.0,1.0),
+          (0.0,1.0)]               
 
+ShortParamNames = ['JuvBase','VrnSens','IndPpSens','ERPpSens']
+
+FittingVariables = ['Lentil.Phenology.StartBuddingDAS','Lentil.Phenology.StartFloweringDAS']
+
+
+# +
 BlankManager = {'$type': 'Models.Manager, Models',
             'CodeArray': [],
             'Parameters': [],
@@ -334,7 +195,9 @@ def CalcScaledValue(Value,RMax,RMin):
 # +
 def Preparefile():
     ## Empty the data store
-    # !del C:\GitHubRepos\ApsimX\Prototypes\Lentil\LentilOptItter.db
+    # #!del C:\GitHubRepos\ApsimX\Prototypes\Lentil\LentilOptItter.db
+    dbpath = WorkingLentilFilePath[:-7]+'.db'
+    if os.path.exists(dbpath): os.remove(dbpath)
     ## Add blank manager into each simulation
     with open(BaseLentilFilePath,'r') as BaseLentilApsimxJSON:
         WorkingLentilApsimx = json.load(BaseLentilApsimxJSON)
@@ -354,14 +217,7 @@ def runModelItter(paramNames,paramValues,FittingVariables,Cultivar,ver):
     ApplyParamReplacementSet(paramValues,paramNames)
     
     OptimisationVariables = ['Observed.'+x for x in FittingVariables]
-    #DataPresent = pd.Series(index = DataTable.index,dtype=bool)
-    #DataPresent = False
-    # for v in OptimisationVariables:
-    #     DataPresent = (DataPresent | ~np.isnan(pd.to_numeric(DataTable.loc[:,v])))
-    SimulationSet = [BaseSimNames[x]+Cultivar for x in range(len(BaseSimNames))]
-    #SimSet = makeLongString(SimulationSet)
     SimSet = '/SimulationNameRegexPattern:('+Cultivar+')'
-    #print(SimSet)
     start = dt.datetime.now()
     subprocess.run(['C:/GitHubRepos/ApsimX/bin/Debug/net6.0/Models.exe',
                     WorkingLentilFilePath,
@@ -417,41 +273,26 @@ def runDevModelItter(paramValues):
     retVal = runModelItter(paramNames,paramValues,FittingVariables,c,Version)
     return retVal
 
-
+# +
+# IttersObsPred = pd.DataFrame(columns = ['ScObs','ScPred','Var','Predicted.Simulation.Name','Predicted.Experiment','Predicted.Wheat.SowingDate',
+#                                                        'NSE','nSims']+paramNames,
+#                                                         index=pd.MultiIndex.from_arrays([[],[],[]],names=['Cultivar','itter','indloc']))
+# itter = 1
+# c='Syrian'
+# paramValues = [814.6453225472756, 0.49836726151624733, 0.8059542544259072, 460.5773909993857, 0.23494631128887233]
+# runDevModelItter(paramValues)
 # -
 
-Version  ='1'
-IttersObsPred = pd.DataFrame(columns = ['ScObs','ScPred','Var','Predicted.Simulation.Name','Predicted.Experiment','Predicted.Wheat.SowingDate',
-                                                       'NSE','nSims']+paramNames,
-                                                        index=pd.MultiIndex.from_arrays([[],[],[]],names=['Cultivar','itter','indloc']))
-itter = 1
-c='Precoz'
-paramValues = [814.6453225472756, 0.49836726151624733, 107.03559095889537, 0.8059542544259072, 460.5773909993857, 0.23494631128887233]
-runDevModelItter(paramValues)
 
-
-Version = '2'
-Cultivars = ['Precoz','Laird']
-#Kappas=pd.Series(index=[0,1,2],data=[100,10,1])
 FailedCultivars = []
-rounds = 0
-while (rounds<10):
+round = 0
+while (round<Rounds):
     for c in Cultivars:
-        print ('Fitting '+c+ ' Round ' + str(rounds))
+        print ('Fitting '+c+ ' Round ' + str(round))
         try:
             checkpoint_saver = CheckpointSaver("./OptFiles/"+c+" FitsCheckpoint"+Version+".pkl", compress=9)
-            RandomCalls = 20
-            OptimizerCalls = 5
             TotalCalls = RandomCalls + OptimizerCalls
-            x0 = [100.0, 0.5, 100.0, 0.5, 100, 0.5]
-            bounds = [(100.0,1000.0),
-                      (0.0,1.0),
-                      (0.0,500.0),
-                      (0.0,1.0),
-                      (100.0,500.0),
-                      (0.0,1.0)]
-
-            if rounds == 0:
+            if round == 0:
                 print ('Creating empty IttersObsPred dataframe')
                 globals()['IttersObsPred'] = pd.DataFrame(columns = ['ScObs','ScPred','Var','SimulationName','ExperimentName','Predicted.Wheat.SowingDate',
                                                        'NSE','nSims']+paramNames,
@@ -470,14 +311,14 @@ while (rounds<10):
                          initial_point_generator='sobol',callback=[checkpoint_saver],x0=x0,y0=y0)
                 currentBest = ret.fun
             print(c)
-            print(str(rounds))
-            print(str(currentBest))
+            print(str(round))
+            print(str(-currentBest))
         except:
-            FailedCultivars.append(-c)
-    rounds += 1
+            FailedCultivars.append(c)
+    round += 1
 
 # +
-ShortParams = pd.Series(index=paramNames+['NSE'],data=['JuvBase','VrnSens','IndBase','IndPpSens','ERbase','ERPpSens','NSE'])
+ShortParams = pd.Series(index=paramNames+['NSE'],data=ShortParamNames+['NSE'])
 
 def plotScObsPred(obsPreSet,alp,leg=True):
     markers = ['o','s','^','v','1','2','3','4','+','x','d']
@@ -526,12 +367,12 @@ def PlotCultivar(obsPreSet,c,ret,ParamCombs):
     plt.ylabel('Best NSE achieved')
     plt.xlabel('Itteration')
     plt.ylim(-2,1)
-    for r in range(0,10):
-        x1 = r * 25 
-        x2 = x1 + 20
+    for r in range(0,Rounds):
+        x1 = r * (RandomCalls +  OptimizerCalls)
+        x2 = x1 + RandomCalls
         plt.fill_between([x1,x2],1,-2,color='b',alpha=0.1)
         x1 = x2
-        x2 + x1+5
+        x2 + x1+RandomCalls
         plt.fill_between([x1,x2],1,-2,color='yellow',alpha=0.1)
     try:
         x0 = list(CampInputs.loc[c,paramNames].values)
@@ -588,14 +429,14 @@ def PlotCultivar(obsPreSet,c,ret,ParamCombs):
         else:
             ax2.axes.yaxis.set_ticklabels([])
         pos+=1
-        
+
 
 
 # -
 
 
-Version = '2'
 BestSet = pd.DataFrame(columns = paramNames+['NSE','Rank','InitAgree','CombScore'])
+endind = len(paramNames) * -1
 for c in Cultivars:
     IttersObsPred = pd.read_pickle("./OptFiles/"+c+"_IttersObsPred"+Version+".pkl")
     ret = load("./OptFiles/"+c+" FitsCheckpoint"+Version+".pkl")
@@ -607,7 +448,7 @@ for c in Cultivars:
     ParamCombs.loc[:,'InitAgree'] = np.nan
     initRank = ParamCombs.loc[0,'Rank']
     for x in ParamCombs.index[:initRank+1]:
-        ParamCombs.loc[x,'InitAgree'] = MUte.MathUtilities.CalcRegressionStats('AS',ParamCombs.loc[0,:][:-5].values,ParamCombs.loc[x,:][:-5].values).NSE
+        ParamCombs.loc[x,'InitAgree'] = MUte.MathUtilities.CalcRegressionStats('AS',ParamCombs.loc[0,:][:endind].values,ParamCombs.loc[x,:][endind].values).NSE
     adequateFits = min(sum(ParamCombs.NSE.values<-0.5),sum(ParamCombs.NSE.values<ParamCombs.loc[bestFit,'NSE']*.8))
     ParamCombs.loc[:,'CombScore']= -ParamCombs.loc[:,'NSE']*ParamCombs.loc[:adequateFits,'InitAgree']
     ParamCombs.sort_values('CombScore',ascending=False,inplace=True)
@@ -618,5 +459,7 @@ for c in Cultivars:
     bestFitObsPred = IttersObsPred.loc[(c,bestFitItter),:]
     PlotCultivar(bestFitObsPred,c,ret,ParamCombs)
     #ParamCombs.sort_values('CombScore',ascending=False,inplace=True)
+
+ParamCombs
 
 BestSet.transpose()
